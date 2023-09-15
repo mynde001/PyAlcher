932b2884babda19d498918c8c08474b6fed75500
Mindaugas <mindaugas.antanas.grigaitis@gmail.com>
2023-09-16 00:55:06 +0300

    Bot will spam high alch without going back to click on spell book

--- main.py
+++ main.py
@@ -3,11 +3,16 @@
 
 import pyautogui
 import sys
+import time
+import threading
 
 from win32gui import FindWindow, GetWindowRect, GetClassName
 
 old_pos = ""
 new_pos = ""
+x, y = "", ""
+
+i = 0
 
 window_handle = FindWindow("SunAwtFrame", None)
 # GetClassName of a client
@@ -16,43 +21,63 @@ window_handle = FindWindow("SunAwtFrame", None)
 old_window_rect_pos = ""
 x0, y0, x1, y1 = "", "", "", ""
 
-def main_fn():
-    global old_pos
-    global new_pos
+def mouse_and_app_coords():
+    global old_pos, new_pos
     global old_window_rect_pos
     global x0, y0, x1, y1
 
-    flag = False
-
     while 1:
-        # Mouse and client coordinates
         new_pos = pyautogui.position()
         x0, y0, x1, y1 = GetWindowRect(window_handle)
 
-        if old_pos != new_pos:
-            sys.stdout.write("\r" + f"x0={str(x0)} y0={str(x1)} x1={str(y0)} y1={str(y1)} {str(new_pos)}")
+        if (old_pos != new_pos):
+            sys.stdout.write("\r" + f"x1={str(x1)} y0={str(y0)} {str(new_pos)}")
             sys.stdout.flush()
 
         old_pos = new_pos
 
         if old_window_rect_pos != (x0, y0, x1, y1):
-            sys.stdout.write("\r" + f"x0={str(x0)} y0={str(x1)} x1={str(y0)} y1={str(y1)} {str(new_pos)}")
+            sys.stdout.write("\r" + f"x1={str(x1)} y0={str(y0)} {str(new_pos)}")
             sys.stdout.flush()
 
         old_window_rect_pos = x0, y0, x1, y1
 
-        # Casting spells
-        if flag == False:
+
+mouse_and_app_coords = threading.Thread(target=mouse_and_app_coords)
+mouse_and_app_coords.start()
+
+def spell_casting():
+    global old_pos, new_pos
+    global x0, y0, x1, y1
+    global x, y
+    global i
+
+    x0, y0, x1, y1 = GetWindowRect(window_handle)
+
+    # Casting spells
+    while i < 3:
+        if (x != x1 - 55 and y != y0 + 335):
             # Spell book
             pyautogui.moveTo(x1 - 25, y0 + 205, 1)
+            time.sleep(0.2)
             pyautogui.click()
-            # High alch spell
+
+            # Move mouse to high alch spell
             pyautogui.moveTo(x1 - 55, y0 + 335, 1)
+
+            # Update mouse position
+            x, y = pyautogui.position()
+
+        if (x == x1 - 55 and y0 + 335):
+            # High alch spell
             pyautogui.click()
+
             # Alch item
+            time.sleep(0.2)
             pyautogui.click()
+            time.sleep(2.5)
 
-            flag = True
+        i += 1
 
 
-main_fn()
+spell_casting()
